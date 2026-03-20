d37cbbef1b08ab1f424d54e6331a6c0c4046967a
Karabo <136013904+99122463500@users.noreply.github.com>
2026-03-20 17:28:39 +0200

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend running on Render");
+});
+
+app.post("/contact", (req, res) => {
+  const { name, email, message } = req.body;
+
+  console.log("Message:", name, email, message);
+
+  res.send("Message sent successfully!");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
